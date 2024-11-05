cf2bce1306556d34a0a1f045b6795963bbd6be09
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2024-11-05 17:40:02 +0100

    feat add: OllamaLLM integration for asking questions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scraper import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from parse import parse_with_ollama
 
 st.title('AI Web Scrapper')
 url = st.text_input('Enter a Website URL: ')
@@ -15,3 +16,15 @@ if st.button('Scrape Site'):
 
     with st.expander('View DOM Content'):
         st.text_area('DOM Content', cleaned_content, height=300)
+
+if 'dom_content' in st.session_state:
+    parse_description = st.text_area('Describe what you want to parse')
+
+    if st.button('Parse Content'):
+        if parse_description:
+            st.write('Parsing the content')
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
